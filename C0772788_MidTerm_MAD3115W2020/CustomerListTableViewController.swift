b1e63a1500bc5a96d9511db327a1effa418ce8a9
Avani Patel <anpatel36@yahoo.in>
2020-03-05 17:59:31 -0500

    [AP] CustomerViewController added

---
+++ C0772788_MidTerm_MAD3115W2020/CustomerListTableViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CustomerListViewController.swift
+//  C0772788_MidTerm_MAD3115W2020
+//
+//  Created by Avani Patel on 3/5/20.
+//  Copyright © 2020 Avani Patel. All rights reserved.
+//
+
+import UIKit
+
+class CustomerListTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
