d4ff66ed4b89b7c9dcea16e8c438cab5640e300b
Avani Patel <anpatel36@yahoo.in>
2020-03-05 20:28:28 -0500

    [AP] Singleton class added

---
+++ C0772788_MidTerm_MAD3115W2020/DataStorage.swift
@@ -0,0 +1,37 @@
+//
+//  DataStorage.swift
+//  C0772788_MidTerm_MAD3115W2020
+//
+//  Created by Avani Patel on 3/5/20.
+//  Copyright © 2020 Avani Patel. All rights reserved.
+//
+
+import Foundation
+class DataStorage
+{
+    private static let dataStorage = DataStorage()
+    private lazy var customerList: [String : Customer] = [:]
+    
+    private init(){}
+       
+    static func getInstance() -> DataStorage
+    {
+        return dataStorage
+    }
+    
+    func addCustomer(customer: Customer) {
+        self.customerList.updateValue(customer, forKey: customer.customerID)
+    }
+    func getAllCustomers() -> [Customer] {
+        return Array(customerList.values);
+    }
+    
+    func loadData()
+    {
+        customerList.updateValue(Customer(customerID: "C001", firstName: "Avani", lastName: "Patel", emailID: "admin@gmail.com"), forKey: "C001")
+         customerList.updateValue(Customer(customerID: "C002", firstName: "Jyothi", lastName: "Thomas", emailID: "admin@gmail.com"), forKey: "C002")
+         customerList.updateValue(Customer(customerID: "C003", firstName: "Charmi", lastName: "Patel", emailID: "admin@gmail.com"), forKey: "C003")
+         customerList.updateValue(Customer(customerID: "C004", firstName: "Shreya", lastName: "Vaghasiya", emailID: "admin@gmail.com"), forKey: "C004")
+         customerList.updateValue(Customer(customerID: "C005", firstName: "Kashyap", lastName: "Zaveri", emailID: "admin@gmail.com"), forKey: "C005")
+    }
+}
