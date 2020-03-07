909e9b65c046a75bfa58c7c91a87ff4f707d0dd9
Avani Patel <anpatel36@yahoo.in>
2020-03-07 17:08:38 -0500

    [AP] function added for email validation in Validation file, Launch Screen optimized

---
+++ C0772788_MidTerm_MAD3115W2020/Validation.swift
@@ -0,0 +1,18 @@
+//
+//  Validation.swift
+//  C0772788_MidTerm_MAD3115W2020
+//
+//  Created by Avani Patel on 3/7/20.
+//  Copyright © 2020 Avani Patel. All rights reserved.
+//
+
+import Foundation
+class validation
+{
+    func isValidEmail() -> Bool
+    {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailTest.evaluate(with: self)
+    }
+}
