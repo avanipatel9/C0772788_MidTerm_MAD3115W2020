516060fcc437b5dbb0260c6be6ecede9264bad4d
Avani Patel <anpatel36@yahoo.in>
2020-03-10 17:26:23 -0400

    [AP] extension added for date formatting

---
+++ C0772788_MidTerm_MAD3115W2020/Extension+Date.swift
@@ -0,0 +1,29 @@
+//
+//  Extension+Date.swift
+//  C0772788_MidTerm_MAD3115W2020
+//
+//  Created by Avani Patel on 3/10/20.
+//  Copyright © 2020 Avani Patel. All rights reserved.
+//
+
+import Foundation
+extension Date
+{
+    public func getForamttedDate() -> String
+    {
+        let dateFormatterPrint = DateFormatter()
+        dateFormatterPrint.dateFormat = "EEEE, dd MMMM, yyyy"
+        let formattedDate = dateFormatterPrint.string(from: self)
+        return formattedDate
+    }
+    
+    static func from(year: Int, month: Int, day: Int) -> Date?
+    {
+        let calendar = Calendar(identifier: .gregorian)
+        var dateComponents = DateComponents()
+        dateComponents.year = year
+        dateComponents.month = month
+        dateComponents.day = day
+        return calendar.date(from: dateComponents) ?? nil
+    }
+}
