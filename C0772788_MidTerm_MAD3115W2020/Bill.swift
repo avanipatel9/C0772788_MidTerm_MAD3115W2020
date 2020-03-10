b5f6fe95eba961aea22c5dbacf3892b82711f29d
Avani Patel <anpatel36@yahoo.in>
2020-03-10 17:07:05 -0400

    [AP] Attributes added in Bill class

--- C0772788_MidTerm_MAD3115W2020/Bill.swift
+++ C0772788_MidTerm_MAD3115W2020/Bill.swift
@@ -7,7 +7,21 @@
 //
 
 import Foundation
+enum BillType
+{
+    case INTERNET, MOBILE, HYDRO, INSURANCE
+}
+
 class Bill
 {
+    var billID: String
+    var billDate: Date
+    var billType: BillType
+    var billAmount: Double = 0.0
     
+    init(billID: String, billDate: Date, billType: BillType) {
+        self.billID = billID
+        self.billDate = billDate
+        self.billType = billType
+    }
 }
