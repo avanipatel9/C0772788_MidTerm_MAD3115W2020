52719fecfeb9d38c82eda3d6bb7bb56b543ccf39
Avani Patel <anpatel36@yahoo.in>
2020-03-10 17:11:59 -0400

    [AP] Attributes added in Internet class

--- C0772788_MidTerm_MAD3115W2020/Internet.swift
+++ C0772788_MidTerm_MAD3115W2020/Internet.swift
@@ -7,7 +7,16 @@
 //
 
 import Foundation
-class Internet
+class Internet: Bill
 {
+    var providerName: String
+    var internetGBUsed: Double
+    var ratePerGB: Double = 7.5
     
+    init(billID: String, billDate: Date, billType: BillType, providerName: String, internetGBUsed: Double)
+    {
+        self.providerName = providerName
+        self.internetGBUsed = internetGBUsed
+        super.init(billID: billID, billDate: billDate, billType: billType)
+    }
 }
