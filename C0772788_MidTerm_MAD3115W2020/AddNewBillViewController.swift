cefb0c1b160d4ec93bb3d7bdccebe911f91277ab
Avani Patel <anpatel36@yahoo.in>
2020-03-13 17:36:23 -0400

    [AP] textfields connected in AddNewBillViewController

--- C0772788_MidTerm_MAD3115W2020/AddNewBillViewController.swift
+++ C0772788_MidTerm_MAD3115W2020/AddNewBillViewController.swift
@@ -11,6 +11,18 @@ import UIKit
 class AddNewBillViewController: UIViewController {
 
     var customer : Customer?
+    
+    @IBOutlet weak var txtBillID: UITextField!
+    @IBOutlet weak var txtBillDate: UITextField!
+    @IBOutlet weak var txtAgencyName: UITextField!
+    @IBOutlet weak var txtUnitConsumed: UITextField!
+    @IBOutlet weak var txtManufacturerName: UITextField!
+    @IBOutlet weak var txtPlanName: UITextField!
+    @IBOutlet weak var txtMobileNumber: UITextField!
+    @IBOutlet weak var txtMobileGBUsed: UITextField!
+    @IBOutlet weak var txtMinutesUsed: UITextField!
+    @IBOutlet weak var txtInternetProviderName: UITextField!
+    @IBOutlet weak var txtInternetGBUsed: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         
